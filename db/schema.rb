45bed91dd5e2ce4c5ececb1447fde5ed9385ed1a
Jesse Shipley <jesse.radack@gmail.com>
2017-01-23 22:43:53 -0700

    add item migration, test, and factory

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170124045814) do
+ActiveRecord::Schema.define(version: 20170124054044) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -21,6 +21,15 @@ ActiveRecord::Schema.define(version: 20170124045814) do
     t.integer  "status",      default: 0
     t.datetime "created_at",              null: false
     t.datetime "updated_at",              null: false
+  end
+
+  create_table "items", force: :cascade do |t|
+    t.string   "name"
+    t.text     "description"
+    t.integer  "unit_price"
+    t.integer  "merchant_id"
+    t.datetime "created_at",  null: false
+    t.datetime "updated_at",  null: false
   end
 
 end
--- spec/factories.rb
+++ spec/factories.rb
@@ -1,4 +1,10 @@
 FactoryGirl.define do
+  factory :item do
+    name "Thing"
+    description "Its a thing! Or something."
+    unit_price 1
+    merchant_id 1
+  end
   factory :invoice do
     customer_id 1
     merchant_id 1
